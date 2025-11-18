f3466b1daf07b06d0a59e3eb5f897ba8a74b187e
cpulidoh <cpulidoh2425@politecnics.barcelona>
2025-11-18 19:26:35 +0100

    18/11/2025

--- Proye_Nano/Form1.cs
+++ Proye_Nano/Form1.cs
@@ -6,5 +6,20 @@ namespace Proye_Nano
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Proye_Nano/obj/Debug/net8.0-windows/Proye_Nano.AssemblyInfo.cs
+++ Proye_Nano/obj/Debug/net8.0-windows/Proye_Nano.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proye_Nano")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddfeca566a4e424315905a01e2c61d4bf773182")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proye_Nano")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proye_Nano")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
